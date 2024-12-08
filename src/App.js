576167e38c5ea08990540fe662f1cdfe9bb57989
troysun95 <sun0531158@gmail.com>
2024-12-08 12:38:27 +0800

    fix (App): fix PrivateRoute ; feat (App) add PublicRoute for setting public routes

--- src/App.js
+++ src/App.js
@@ -12,9 +12,13 @@ function App() {
   const {
     isAuthenticated
   } = useAuth();
-
+  
   const PrivateRoute = ({children})=>{
     return isAuthenticated ? children : <Navigate to='/login' />
+  }
+
+  const PublicRoute = ({children})=>{
+    return isAuthenticated ? <Navigate to='/home' replace/> : children
   }
 
   //檢驗現有驗證狀態
@@ -26,13 +30,27 @@ function App() {
       <BrowserRouter>
         <div className="app">
           <Routes>
+            {/* 私路由 */}
             <Route path="/home" element={
               <PrivateRoute>
                 <HomePage/>
               </PrivateRoute>
             }/>
-            <Route path="/login" element={isAuthenticated ? <HomePage/> : <LoginPage/>}/>
-            <Route path="/signup" element={<SignupPage/>}/>
+
+            {/* <Route path="/login" element={isAuthenticated ? <HomePage/> : <LoginPage/>}/>
+            <Route path="/signup" element={<SignupPage/>}/> */}
+
+            {/* 公路由 */}
+            <Route path="/login" element ={
+              <PublicRoute>
+                <LoginPage/>
+              </PublicRoute>
+            }/>
+             <Route path="/signup" element ={
+              <PublicRoute>
+                <SignupPage/>
+              </PublicRoute>
+            }/>
             <Route path="*" element={<ErrorPage />} />
           </Routes>
         </div>
